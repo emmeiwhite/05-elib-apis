8b6bbe42269cb8b461cf575e9fe05a5fe4c3ab17
emmeiwhite <emmeiwhite@gmail.com>
2025-05-26 21:26:07 +0530

    elib-3.8 login user with accessToken generation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import { config } from "../config/config";
 
 import { sign } from "jsonwebtoken";
+import { create } from "domain";
 
 // 1. Register User
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -63,9 +64,43 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
 // 2. Login User
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({
-    message: "User Successfully logged in",
-  });
+  const { email, password } = req.body;
+
+  // 1. simple validation
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  // 2. Query the database
+  let user;
+  try {
+    user = await userModal.findOne({ email });
+    if (!user) {
+      return next(createHttpError(404, "User not found"));
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting the user"));
+  }
+
+  //3. Compare  password received from user with database password
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "UserName or password incorrect"));
+  }
+
+  // On Match (isMatch = true), we generate a new access token
+
+  try {
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+    // 3. Response
+    res.status(200).json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Error while signing the jwt token"));
+  }
 };
 
 export { createUser, loginUser };
