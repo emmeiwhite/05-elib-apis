2ccee98724906fe7121dce0cb4e4850947f975b6
emmeiwhite <emmeiwhite@gmail.com>
2025-05-26 16:19:18 +0530

    elib-3.2 password hashing done with bcrypt

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import { userModal } from "./userModel";
-
+import bcrypt from "bcrypt";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
   // 1. Validation
@@ -19,11 +19,27 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "User already exists with this email");
     return next(error);
   }
+
   // 2. Process
 
-  // 3. Response
+  /// Now we need to create user and store in the database, but before that we require to hash the password because passwords are not stored in the raw form as they come.
 
-  res.json({ message: "User created" });
+  // Hash password
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  /** Time to store user in the database */
+  // Save user
+  const newUser = await userModal.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  /** Because we send back the response, we need to generate Token */
+
+  // 3. Response
+  res.json({ id: newUser._id });
 };
 
 export { createUser };
